e3cf93e0cb34e6ec17e43ed7bcb8003da2052cc8
임소영 <s2412@e-mirim.hs.kr>
2025-02-06 15:00:53 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.51 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length-5];
-        int temp = 0;
         Arrays.sort(num_list);
         
-        for(int i=5; i<num_list.length; i++){
-            answer[temp++] = num_list[i];
+        for(int i=0; i<num_list.length-5; i++){
+            answer[i] = num_list[i+5];
         }
+        
         return answer;
     }
 }
