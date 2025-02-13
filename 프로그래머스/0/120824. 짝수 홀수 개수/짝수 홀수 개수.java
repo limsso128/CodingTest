454d53945b0f23164b2857da88e2cac0baf4a35f
임소영 <s2412@e-mirim.hs.kr>
2025-02-13 15:59:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,18 +1,10 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int odd = 0;
-        int even = 0;
-
+        int[] answer = new int[2];
+        
         for(int i=0; i<num_list.length; i++){
-            if(num_list[i]%2==0){
-                even++;
-            }else{
-                odd++;
-            }
+            answer[num_list[i]%2]++;
         }
-        
-        int[] answer = {even, odd};
-        
         return answer;
     }
 }
